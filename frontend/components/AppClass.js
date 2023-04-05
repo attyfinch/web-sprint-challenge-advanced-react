f7933328eb9fcc68ea0083b4d13f7964aa51dcf1
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-04-05 09:13:39 -0700

    wrapping up bug fixes

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import axios from 'axios'
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
+const initialX = 2
+const initialY = 2
 
 const initialState = {
   message: initialMessage,
@@ -13,7 +16,22 @@ const initialState = {
   steps: initialSteps,
 }
 
+const URL = 'http://localhost:9000/api/result'
+
+
 export default class AppClass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: initialMessage,
+      email: initialEmail,
+      index: initialIndex,
+      steps: initialSteps,
+      x: initialX,
+      y: initialY
+    }
+  }
+
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
@@ -30,25 +48,92 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     // Use this helper to reset all states to their initial values.
+    this.setState({...this.setState, message: initialMessage})
+    this.setState({...this.setState, email: initialEmail})
+    this.setState({...this.setState, index: initialIndex})
+    this.setState({...this.setState, steps: initialSteps})
+    this.setState({...this.setState, x: initialX})
+    this.setState({...this.setState, y: initialY})
   }
 
   getNextIndex = (direction) => {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+
+    if (this.state.index === 0 && direction === 'up') { this.setState({...this.setState, message: "You can't go up", steps: this.state.steps + 1}) }
+    if (this.state.index === 0 && direction === 'right') { this.setState({...this.setState, index: 1, message: "", steps: this.state.steps + 1, x: 2, y: 1 }) }
+    if (this.state.index === 0 && direction === 'down') { this.setState({...this.setState, index: 3, message: "", steps: this.state.steps + 1, x: 1, y: 2 }) }
+    if (this.state.index === 0 && direction === 'left') { this.setState({...this.setState, message: "You can't go left"}) }
+
+    if (this.state.index === 1 && direction === 'up') { this.setState({...this.setState, message: "You can't go up"}) }
+    if (this.state.index === 1 && direction === 'right') { this.setState({...this.setState, index: 2, message: "", steps: this.state.steps+1, x: 3, y: 1}) }
+    if (this.state.index === 1 && direction === 'down') { this.setState({...this.setState, index: 4, message: "", steps: this.state.steps+1, x: 2, y: 2}) }
+    if (this.state.index === 1 && direction === 'left') { this.setState({...this.setState, index: 0, message: "", steps: this.state.steps+1, x: 1, y: 1}) }
+
+    if (this.state.index === 2 && direction === 'up') { this.setState({...this.setState, message: "You can't go up"}) }
+    if (this.state.index === 2 && direction === 'right') { this.setState({...this.setState, message: "You can't go right"}) }
+    if (this.state.index === 2 && direction === 'down') { this.setState({...this.setState, index: 5, message: "", steps: this.state.steps+1, x: 3, y: 2}) }
+    if (this.state.index === 2 && direction === 'left') { this.setState({...this.setState, index: 1, message: "", steps: this.state.steps+1, x: 2, y: 1}) }
+
+    if (this.state.index === 3 && direction === 'up') { this.setState({...this.setState, index: 0, message: "", steps: this.state.steps+1, x: 1, y: 1}) }
+    if (this.state.index === 3 && direction === 'right') { this.setState({...this.setState, index: 4, message: "", steps: this.state.steps+1, x: 2, y: 2}) }
+    if (this.state.index === 3 && direction === 'down') { this.setState({...this.setState, index: 6, message: "", steps: this.state.steps+1, x: 1, y: 3}) }
+    if (this.state.index === 3 && direction === 'left') { this.setState({...this.setState, message: "You can't go left" }) }
+
+
+    if (this.state.index === 4 && direction === 'up') { this.setState({...this.setState, index: 1, message: "", steps: this.state.steps + 1, x: 2, y: 1}) }
+    if (this.state.index === 4 && direction === 'right') { this.setState({...this.setState, index: 5, message: "", steps: this.state.steps + 1, x: 3, y: 2}) }
+    if (this.state.index === 4 && direction === 'down') { this.setState({...this.setState, index: 7, message: "", steps: this.state.steps + 1, x: 2, y: 3}) }
+    if (this.state.index === 4 && direction === 'left') { this.setState({...this.setState, index: 3, message: "", steps: this.state.steps + 1, x: 1, y: 2}) }
+
+    if (this.state.index === 5 && direction === 'up') { this.setState({...this.setState, index: 2, message: "", steps: this.state.steps+1, x: 3, y: 1}) }
+    if (this.state.index === 5 && direction === 'right') { this.setState({...this.setState, message: "You can't go right"}) }
+    if (this.state.index === 5 && direction === 'down') { this.setState({...this.setState, index: 8, message: "", steps: this.state.steps+1, x: 3, y: 3}) }
+    if (this.state.index === 5 && direction === 'left') { this.setState({...this.setState, index: 4, message: "", steps: this.state.steps+1, x: 2, y: 2}) }
+
+    if (this.state.index === 6 && direction === 'up') { this.setState({...this.setState, index: 3, message: "", steps: this.state.steps+1, x: 1, y: 2}) }
+    if (this.state.index === 6 && direction === 'right') { this.setState({...this.setState,index: 7, message: "", steps: this.state.steps+1, x: 2, y: 3}) }
+    if (this.state.index === 6 && direction === 'down') { this.setState({...this.setState,  message: "You can't go down"}) }
+    if (this.state.index === 6 && direction === 'left') { this.setState({...this.setState,  message: "You can't go left"}) }
+
+    if (this.state.index === 7 && direction === 'up') { this.setState({...this.setState, index: 4, message: "", steps: this.state.steps+1, x: 2, y: 2}) }
+    if (this.state.index === 7 && direction === 'right') { this.setState({...this.setState, index: 8, message: "", steps: this.state.steps+1, x: 3, y: 3}) }
+    if (this.state.index === 7 && direction === 'down') { this.setState({...this.setState, message: "You can't go down"}) }
+    if (this.state.index === 7 && direction === 'left') { this.setState({...this.setState, index: 6, message: "", steps: this.state.steps+1, x: 1, y: 3}) }
+
+    if (this.state.index === 8 && direction === 'up') { this.setState({...this.setState, index: 5, message: "", steps: this.state.steps+1, x: 3, y: 2}) }
+    if (this.state.index === 8 && direction === 'right') { this.setState({...this.setState, message: "You can't go right"}) }
+    if (this.state.index === 8 && direction === 'down') { this.setState({...this.setState, message: "You can't go down"}) }
+    if (this.state.index === 8 && direction === 'left') { this.setState({...this.setState, index: 7, message: "", steps: this.state.steps+1, x: 2, y: 3}) }
   }
 
-  move = (evt) => {
+  move = (e) => {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    this.getNextIndex(e.target.id)
   }
 
-  onChange = (evt) => {
+  onChange = (e) => {
     // You will need this to update the value of the input.
+    e.preventDefault();
+    this.setState({...this.setState, email: e.target.value});
   }
 
-  onSubmit = (evt) => {
+  onSubmit = (e) => {
     // Use a POST request to send a payload to the server.
+    e.preventDefault();
+    axios.post(URL, { "x": this.state.x, "y": this.state.y, "steps": this.state.steps, "email": this.state.email })
+    .then(res => {
+      console.log(res)
+      this.setState({...this.setState, message: res.data.message})
+    })
+    .catch(err => {
+      console.log('something is wrong')
+    })
+
+    this.setState({...this.setState, email: initialEmail})
+
   }
 
   render() {
@@ -56,30 +141,30 @@ export default class AppClass extends React.Component {
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">Coordinates ({this.state.x}, {this.state.y})</h3>
+          <h3 id="steps">You moved {this.state.steps} times</h3>
         </div>
         <div id="grid">
           {
             [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-              <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-                {idx === 4 ? 'B' : null}
+              <div key={idx} className={`square${idx === this.state.index ? ' active' : ''}`}>
+                {idx === this.state.index ? 'B' : null}
               </div>
             ))
           }
         </div>
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.move}>LEFT</button>
+          <button id="up" onClick={this.move}>UP</button>
+          <button id="right" onClick={this.move}>RIGHT</button>
+          <button id="down" onClick={this.move}>DOWN</button>
+          <button id="reset" onClick={this.reset}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.onSubmit}>
+          <input id="email" type="email" placeholder="type email" onChange={this.onChange} value={this.state.email}></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
+const initialX = 2;
+const initialY = 2;
+const grid = ["(1, 1)", "(1, 2)", "(1, 3)", "(2, 1)", "(2, 2)", "(2, 3)", "(3, 1)", "(3, 2)", "(3, 3)"];
+const URL = 'http://localhost:9000/api/result'
+
 
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [message, setMessage] = useState(initialMessage);
+  const [email, setEmail] = useState(initialEmail);
+  const [index, setIndex] = useState(initialIndex);
+  const [steps, setSteps] = useState(initialSteps);
+  const [x, setX] = useState(initialX);
+  const [y, setY] = useState(initialY);
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
@@ -23,54 +37,124 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setMessage(initialMessage);
+    setEmail(initialEmail);
+    setIndex(initialIndex);
+    setSteps(initialSteps);
+    setX(initialX);
+    setY(initialY);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+
+    if (index === 0 && direction === 'up') { setMessage("You can't go up"); setSteps(steps+1) }
+    if (index === 0 && direction === 'right') { setIndex(1); setMessage(""); setSteps(steps+1); setX(2); setY(1) }
+    if (index === 0 && direction === 'down') { setIndex(3); setMessage(""); setSteps(steps+1); setX(1); setY(2) }
+    if (index === 0 && direction === 'left') { setMessage("You cant go left") }
+
+    if (index === 1 && direction === 'up') { setMessage("You can't go up") }
+    if (index === 1 && direction === 'right') { setIndex(2); setMessage(""); setSteps(steps+1); setX(3); setY(1) }
+    if (index === 1 && direction === 'down') { setIndex(4); setMessage(""); setSteps(steps+1); setX(2); setY(2) }
+    if (index === 1 && direction === 'left') { setIndex(0); setMessage(""); setSteps(steps+1); setX(1); setY(1) }
+
+    if (index === 2 && direction === 'up') { setMessage("You can't go up") }
+    if (index === 2 && direction === 'right') { setMessage("You can't go right") }
+    if (index === 2 && direction === 'down') { setIndex(5); setMessage(""); setSteps(steps+1); setX(3); setY(2) }
+    if (index === 2 && direction === 'left') { setIndex(1); setMessage(""); setSteps(steps+1); setX(2); setY(1) }
+
+    if (index === 3 && direction === 'up') { setIndex(0); setMessage(""); setSteps(steps+1); setX(1); setY(1) }
+    if (index === 3 && direction === 'right') { setIndex(4); setMessage(""); setSteps(steps+1); setX(2); setY(2) }
+    if (index === 3 && direction === 'down') { setIndex(6); setMessage(""); setSteps(steps+1); setX(1); setY(3) }
+    if (index === 3 && direction === 'left') { setMessage("You can't go left") }
+
+    if (index === 4 && direction === 'up') { setIndex(1); setMessage(""); setSteps(steps+1); setX(1); setY(2) }
+    if (index === 4 && direction === 'right') { setIndex(5); setMessage(""); setSteps(steps+1); setX(3); setY(2) }
+    if (index === 4 && direction === 'down') { setIndex(7); setMessage(""); setSteps(steps+1); setX(2); setY(3) }
+    if (index === 4 && direction === 'left') { setIndex(3); setMessage(""); setSteps(steps+1); setX(1); setY(2) }
+
+    if (index === 5 && direction === 'up') { setIndex(2); setMessage(""); setSteps(steps+1); setX(3); setY(1) }
+    if (index === 5 && direction === 'right') { setMessage("You can't go right") }
+    if (index === 5 && direction === 'down') { setIndex(8); setMessage(""); setSteps(steps+1); setX(3); setY(3) }
+    if (index === 5 && direction === 'left') { setIndex(4); setMessage(""); setSteps(steps+1); setX(2); setY(2) }
+
+    if (index === 6 && direction === 'up') { setIndex(3); setMessage(""); setSteps(steps+1); setX(1); setY(2) }
+    if (index === 6 && direction === 'right') { setIndex(7); setMessage(""); setSteps(steps+1); setX(2); setY(3) }
+    if (index === 6 && direction === 'down') { setMessage("You can't go down") }
+    if (index === 6 && direction === 'left') { setMessage("You can't go left") }
+
+    if (index === 7 && direction === 'up') { setIndex(4); setMessage(""); setSteps(steps+1); setX(2); setY(2) }
+    if (index === 7 && direction === 'right') { setIndex(8); setMessage(""); setSteps(steps+1); setX(3); setY(3) }
+    if (index === 7 && direction === 'down') { setMessage("You can't go down") }
+    if (index === 7 && direction === 'left') { setIndex(6); setMessage(""); setSteps(steps+1); setX(1); setY(3) }
+
+    if (index === 8 && direction === 'up') { setIndex(5); setMessage(""); setSteps(steps+1); setX(3); setY(2) }
+    if (index === 8 && direction === 'right') { setMessage("You can't go right") }
+    if (index === 8 && direction === 'down') { setMessage("You can't go down") }
+    if (index === 8 && direction === 'left') { setIndex(7); setMessage(""); setSteps(steps+1); setX(2); setY(3) }
   }
 
-  function move(evt) {
+  function move(e) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    getNextIndex(e.target.id)
   }
 
-  function onChange(evt) {
+  function onChange(e) {
     // You will need this to update the value of the input.
+    console.log(e)
+    e.preventDefault();
+    setEmail(e.target.value);
+    if (e.target.validity.valid === false) {
+      setMessage("Ouch: email must be a valid email")
+    }
+
   }
 
-  function onSubmit(evt) {
-    // Use a POST request to send a payload to the server.
+  function onSubmit(e) {
+    console.log(e)
+    e.preventDefault();
+    axios.post(URL, { "x": x, "y": y, "steps": steps, "email": email })
+    .then(res => {
+      console.log(res)
+      setMessage(res.data.message)
+    })
+    .catch(err => {
+      setMessage("Ouch: email is required")
+    })
+
+    setEmail(initialEmail)
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">Coordinates ({x}, {y})</h3>
+        <h3 id="steps">You moved {steps} times</h3>
       </div>
       <div id="grid">
         {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
+          grid.map((ele, idx) => (
+            <div key={idx} className={`square${idx === index ? ' active' : ''}`}>
+              {idx === index ? 'B' : null}
             </div>
           ))
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={move}>LEFT</button>
+        <button id="up" onClick={move}>UP</button>
+        <button id="right" onClick={move}>RIGHT</button>
+        <button id="down" onClick={move}>DOWN</button>
+        <button id="reset" onClick={reset}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
+      <form onSubmit={onSubmit}>
+        <input id="email" type="email" placeholder="type email" onChange={onChange} value={email}></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>

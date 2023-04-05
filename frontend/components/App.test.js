c61a69f1873e4ce7b82e77b20f317964d4801db7
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-04-05 11:50:48 -0700

    testing tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import AppFunctional from './frontend/components/AppFunctional'
-import AppClass from './frontend/components/AppClass'
-import { render, fireEvent, screen } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
+// import AppFunctional from './frontend/components/AppFunctional'
+// import AppClass from './frontend/components/AppClass'
+// import { render, fireEvent, screen } from '@testing-library/react'
+// import '@testing-library/jest-dom/extend-expect'
 
 
 // Write your tests here

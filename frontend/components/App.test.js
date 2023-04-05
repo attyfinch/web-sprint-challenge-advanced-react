cefae03aa9999461741e95e496cc2dc8e82686da
Brad Morris <bradmorris@Brads-Air.attlocal.net>
2023-04-05 11:41:21 -0700

    testing test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,24 @@
+import server from './backend/mock-server'
+import React from 'react'
+import AppFunctional from './frontend/components/AppFunctional'
+import AppClass from './frontend/components/AppClass'
+import { render, fireEvent, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  render(<AppFunctional />)
+  expect(true).toBe(true)
 })
+
+
+// test('starting position and text correct', () => {
+//   render(<AppFunctional />)
+
+//   const header = screen.getByText("Welcome the GRID");
+//   expect(header).toBeInTheDocument();
+
+// })
+
+
